611006c118e3c6051e2442d8e855ca0d8bc6ffa9
Ivan <ivankazachenko@mail.ru>
2025-12-07 23:57:01 +0700

    main

--- app/mod/wb/wb.db..js
+++ app/mod/wb/wb.db..js
@@ -53,9 +53,7 @@ const setDelivery = async (obj) => {
     if (typeof obj !== 'object') throw 'error неверные данные'
     for (const dataWb of Object.values(obj)) {
       const { warehouseId, date } = dataWb[0]
-      const dataDb = await knex('wbDelivery').where('warehouseId', '=', warehouseId).and.where('date', '=', date).orderBy('deliveryId')
-      // const dataDb = await utils.pg.get('wbDelivery', { warehouseId, date }, { orderBy: { deliveryId: true } })
-
+      const dataDb = await knex('wbDelivery').where('warehouseId', '=', warehouseId).and.where('date', '=', date).orderBy('deliveryId', 'asc')
 
       const num = dataWb.length > dataDb.length
         ? dataWb.length
@@ -66,18 +64,15 @@ const setDelivery = async (obj) => {
           const { deliveryId } = dataDb[i]
           // console.log('set', deliveryId)
           await knex('wbDelivery').where('deliveryId', '=', deliveryId).update(dataWb[i])
-          // utils.pg.set('wbDelivery', { deliveryId }, dataWb[i])
 
         } else if (typeof dataWb[i] === 'undefined') {
           const { deliveryId } = dataDb[i]
           // console.log('del', deliveryId)
           await knex('wbDelivery').where('deliveryId', '=', deliveryId).delete()
-          // utils.pg.del('wbDelivery', { deliveryId })
 
         } else if (typeof dataDb[i] === 'undefined') {
           // console.log('add')
           await knex('wbDelivery').insert(dataWb[i])
-          // utils.pg.add('wbDelivery', dataWb[i])
 
         }
       }
